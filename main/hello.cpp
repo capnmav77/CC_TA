5d4c857e513e97ed32758dcca5ec106f85076154
PES1UG22CS157 <151386309+Chethank-03@users.noreply.github.com>
2025-03-11 20:54:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this\n";
+  cout << "Webhook test successful!\n";
   return 0;
 }
