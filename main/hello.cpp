33c492c432d323479425ef208c9cb8461fc5d245
Chethank-03 <chethan.k000333@gmial.com>
2025-03-11 22:58:28 +0530

    added webhook test message to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this\n";
-  cout << "Webhook test successful!\n";
+  cout << "Webhook test successful!\n";  // Added webhook test message
   return 0;
 }
